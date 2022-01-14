c6b245c6a0aad57f56103c6fe558465e360f2684
fiftyeightandeight <alexd@alexgo.io>
2022-01-14 18:14:40 +0800

    agp005/006 tests added

--- tests/dao_test.ts
+++ tests/dao_test.ts
@@ -4,8 +4,11 @@ import { Clarinet, Tx, Chain, Account, types } from 'https://deno.land/x/clarine
 
 const daoAddress = "ST1HTBVD3JG9C05J7HBJTHGR0GGW7KXW28M5JS8QE.executor-dao";
 const bootstrapAddress = "ST1HTBVD3JG9C05J7HBJTHGR0GGW7KXW28M5JS8QE.agp000-bootstrap";
-const agp002Address = "ST1HTBVD3JG9C05J7HBJTHGR0GGW7KXW28M5JS8QE.agp002-launchpad-alex";
 const age000Address = "ST1HTBVD3JG9C05J7HBJTHGR0GGW7KXW28M5JS8QE.age000-governance-token";
+
+const agp003Address = "ST1HTBVD3JG9C05J7HBJTHGR0GGW7KXW28M5JS8QE.agp003-launchpad-alex";
+const agp005Address = "ST1HTBVD3JG9C05J7HBJTHGR0GGW7KXW28M5JS8QE.agp005-wstx-alex-50-50";
+const agp006Address = "ST1HTBVD3JG9C05J7HBJTHGR0GGW7KXW28M5JS8QE.agp006-flash-loan-fee";
 
 class DAO {
     chain: Chain;
@@ -33,6 +36,18 @@ class DAO {
       ]);
       return block.receipts[0].result;      
     }  
+
+    transferToken(sender: Account, token: string, amount: number, receiver: string, memo:ArrayBuffer) {
+      let block = this.chain.mineBlock([
+          Tx.contractCall(token, "transfer-fixed", [
+            types.uint(amount),
+            types.principal(sender.address),
+            types.principal(receiver),
+            types.some(types.buff(memo))
+          ], sender.address),
+        ]);
+        return block.receipts[0].result;
+    }   
 }
 
 /**
@@ -41,7 +56,7 @@ class DAO {
  */
 
 Clarinet.test({
-    name: "DAO: bootstrap",
+    name: "DAO: agp003",
 
     async fn(chain: Chain, accounts: Map<string, Account>) {
         let deployer = accounts.get("deployer")!;
@@ -50,7 +65,7 @@ Clarinet.test({
         let result:any = await DAOTest.construct(deployer, bootstrapAddress);
         result.expectOk();
 
-        result = await DAOTest.executiveAction(deployer, agp002Address);
+        result = await DAOTest.executiveAction(deployer, agp003Address);
         result.expectOk();
 
         let call = chain.callReadOnlyFn("alex-launchpad", "get-listing-details", [ types.principal(age000Address)], deployer.address);
@@ -58,3 +73,63 @@ Clarinet.test({
       }
     },    
 );
+
+Clarinet.test({
+  name: "DAO: agp005",
+
+  async fn(chain: Chain, accounts: Map<string, Account>) {
+      let deployer = accounts.get("deployer")!;
+      let DAOTest = new DAO(chain, deployer);
+
+      let result:any = await DAOTest.construct(deployer, bootstrapAddress);
+      result.expectOk();
+
+      result = await DAOTest.transferToken(deployer, 'token-wstx', 1_000_000e8, daoAddress, new ArrayBuffer(4));
+      result.expectOk();
+
+      result = await DAOTest.executiveAction(deployer, agp005Address);
+      result.expectOk();
+
+      let call = chain.callReadOnlyFn("fixed-weight-pool", "get-pool-contracts",[ types.uint(1) ], deployer.address);
+      result = call.result.expectOk().expectTuple();
+      result['token-x'].expectPrincipal(deployer.address + '.token-wstx');
+      result['token-y'].expectPrincipal(deployer.address + '.age000-governance-token');      
+      result['weight-x'].expectUint(0.5e8);
+      result['weight-y'].expectUint(0.5e8);
+
+      call = chain.callReadOnlyFn("fixed-weight-pool", "get-pool-details", 
+      [ 
+        types.principal(deployer.address + '.token-wstx'),
+        types.principal(deployer.address + '.age000-governance-token'),
+        types.uint(0.5e8),
+        types.uint(0.5e8)
+      ], deployer.address);
+      result = call.result.expectOk().expectTuple();
+      result['fee-rebate'].expectUint(0.5e8);
+      result['fee-rate-x'].expectUint(0.003e8);
+      result['fee-rate-y'].expectUint(0.003e8);
+      result['oracle-average'].expectUint(0.95e8);
+    }
+  },    
+);
+
+Clarinet.test({
+  name: "DAO: agp006",
+
+  async fn(chain: Chain, accounts: Map<string, Account>) {
+      let deployer = accounts.get("deployer")!;
+      let DAOTest = new DAO(chain, deployer);
+
+      let result:any = await DAOTest.construct(deployer, bootstrapAddress);
+      result.expectOk();
+
+      result = await DAOTest.executiveAction(deployer, agp006Address);
+      result.expectOk();
+
+      let call = chain.callReadOnlyFn("alex-vault", "get-flash-loan-fee-rate", [], deployer.address);
+      call.result.expectOk().expectUint(0.003e8);
+    }
+  },    
+);
+
+
