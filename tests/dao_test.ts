2512ffa26349573a1502b59d7c138f9a353e52ba
fiftyeightandeight <alexd@alexgo.io>
2022-04-01 14:31:53 +0800

    agp039 test suite added

--- tests/dao_test.ts
+++ tests/dao_test.ts
@@ -7,19 +7,18 @@ import {
 } from "https://deno.land/x/clarinet@v0.14.0/index.ts";
 import { assertEquals } from 'https://deno.land/std@0.90.0/testing/asserts.ts';
 
-const daoAddress = "ST1HTBVD3JG9C05J7HBJTHGR0GGW7KXW28M5JS8QE.executor-dao";
-const bootstrapAddress =
-  "ST1HTBVD3JG9C05J7HBJTHGR0GGW7KXW28M5JS8QE.agp000-bootstrap";
-const agp029Address =
-  "ST1HTBVD3JG9C05J7HBJTHGR0GGW7KXW28M5JS8QE.agp029";               
-const agp030Address =
-  "ST1HTBVD3JG9C05J7HBJTHGR0GGW7KXW28M5JS8QE.agp030";   
-const agp031Address =
-  "ST1HTBVD3JG9C05J7HBJTHGR0GGW7KXW28M5JS8QE.agp031";   
-const agp035Address =
-  "ST1HTBVD3JG9C05J7HBJTHGR0GGW7KXW28M5JS8QE.agp035";                   
+const deployerAddress = "ST1HTBVD3JG9C05J7HBJTHGR0GGW7KXW28M5JS8QE";
 
-
+const daoAddress = deployerAddress + ".executor-dao";
+const bootstrapAddress = deployerAddress + ".agp000-bootstrap";
+const agp029Address = deployerAddress + ".agp029";               
+const agp030Address = deployerAddress + ".agp030";   
+const agp031Address = deployerAddress + ".agp031";   
+const agp035Address = deployerAddress + ".agp035";    
+const agp036Address = deployerAddress + ".agp036";   
+const agp039Address = deployerAddress + ".agp039";   
+const age004Address = deployerAddress + ".age004-claim-and-stake";
+const age003Address = deployerAddress + ".age003-emergency-execute";
 class DAO {
   chain: Chain;
   deployer: Account;
@@ -149,7 +148,7 @@ Clarinet.test({
 });
 
  Clarinet.test({
-  name: "DAO: agp035",
+  name: "DAO: agp035/36",
 
   async fn(chain: Chain, accounts: Map<string, Account>) {
     let deployer = accounts.get("deployer")!;
@@ -167,6 +166,56 @@ Clarinet.test({
     result.expectOk();    
 
     result = await DAOTest.executiveAction(deployer, agp035Address);
-    console.log(result);
+    result.expectOk();
+    result = await DAOTest.executiveAction(deployer, agp036Address);
+    result.expectOk();
+  },
+});
+
+Clarinet.test({
+  name: "DAO: agp039",
+
+  async fn(chain: Chain, accounts: Map<string, Account>) {
+    let deployer = accounts.get("deployer")!;
+    let DAOTest = new DAO(chain, deployer);
+
+    let result: any = await DAOTest.construct(deployer, bootstrapAddress);
+    result.expectOk(); 
+
+    result = await DAOTest.executiveAction(deployer, agp039Address);
+    result.expectOk();
+  },
+});
+
+Clarinet.test({
+  name: "DAO: agp039",
+
+  async fn(chain: Chain, accounts: Map<string, Account>) {
+    let deployer = accounts.get("deployer")!;
+    let DAOTest = new DAO(chain, deployer);
+
+    let result: any = await DAOTest.construct(deployer, bootstrapAddress);
+    result.expectOk(); 
+
+    result = await DAOTest.executiveAction(deployer, agp039Address);
+    result.expectOk();
+
+    let call: any = chain.callReadOnlyFn("age004-claim-and-stake", "buff-to-uint", [types.buff(new Uint8Array([0x01]).buffer)], deployer.address);
+    call.result.expectUint(1);
+
+    result = chain.mineBlock([
+      Tx.contractCall("age004-claim-and-stake", "claim-and-stake", 
+      [
+        types.principal(age003Address),
+        types.buff(new Uint8Array([0x01]).buffer)
+      ], deployer.address),
+      Tx.contractCall("age004-claim-and-stake", "claim-and-stake", 
+      [
+        types.principal(age004Address),
+        types.buff(new Uint8Array([0x01]).buffer)
+      ], deployer.address)      
+    ]);
+    result.receipts[0].result.expectErr().expectUint(3000);
+    result.receipts[1].result.expectErr().expectUint(2026);
   },
 });
