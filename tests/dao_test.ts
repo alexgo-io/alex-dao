4ec050c0962e52f6cad9069d394df7159fc2598e
fiftyeightandeight <alexd@alexgo.io>
2022-06-07 14:20:37 +0800

    agp063 test added

--- tests/dao_test.ts
+++ tests/dao_test.ts
@@ -512,3 +512,18 @@ Clarinet.test({
     result.expectOk();
   },
 });
+
+Clarinet.test({
+  name: "DAO: agp063",
+
+  async fn(chain: Chain, accounts: Map<string, Account>) {
+    let deployer = accounts.get("deployer")!;
+    let DAOTest = new DAO(chain, deployer);
+
+    let result: any = await DAOTest.construct(deployer, bootstrapAddress);
+    result.expectOk(); 
+
+    result = await DAOTest.executiveAction(deployer, deployer.address + ".agp063");
+    result.expectOk();
+  },
+});
