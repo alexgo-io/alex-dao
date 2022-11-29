aecb36319fdf7c2b3a870c66098565acec15a0ce
fiftyeightandeight <alexd@alexgo.io>
2022-11-29 19:18:12 +0800

    agp099 added

--- tests/dao_test.ts
+++ tests/dao_test.ts
@@ -738,7 +738,7 @@ class DAO {
 // });
 
 Clarinet.test({
-  name: "DAO: age009, agp091, agp092, agp093, agp094, agp097, agp098",
+  name: "DAO: age009, agp091, agp092, agp093, agp094, agp097, agp098, agp099",
 
   async fn(chain: Chain, accounts: Map<string, Account>) {
     let deployer = accounts.get("deployer")!;
@@ -773,6 +773,9 @@ Clarinet.test({
 
     result = await DAOTest.executiveAction(deployer, deployer.address + ".agp098");
     result.expectOk();          
+
+    result = await DAOTest.executiveAction(deployer, deployer.address + ".agp099");
+    result.expectOk();              
 
     console.log(result);
   },
